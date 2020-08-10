e9bd0f6c478530f0888723e591e5a7699d6dfc8a
Ariel Sanchez <arii.sanchez123@gmail.com>
2020-08-10 18:21:58 -0300

    Eliminacion de pedidos (admin)

--- routes/orders.js
+++ routes/orders.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { isLogged, isAdmin } = require('../middlewares/global');
-const { post, get, put } = require('../middlewares/orders');
+const { post, get, put, deleteOrder } = require('../middlewares/orders');
 
 router.route('/')
   .post(isLogged, post.hasProducts, post.hasQuantity, post.productsExist, post.newOrder)
@@ -8,6 +8,7 @@ router.route('/')
 
 router.route('/:id')
   .put(isLogged, isAdmin, put.verifyStatus, put.orderExist, put.update)
+  .delete(isLogged, isAdmin, deleteOrder.exist, deleteOrder.delete)
 
 
 module.exports = router;
